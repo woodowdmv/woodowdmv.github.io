c3a4015750dee45a25761b6db8a10619292ee177
Jingning <zhangjn1121@gmail.com>
2020-06-14 20:32:45 +0800

    添加日后谈

--- ArticleFetcher.js
+++ ArticleFetcher.js
@@ -1,5 +1,3 @@
-let blockRows = 7;
-
 function GetQueryString(name)
 {
     let reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
@@ -9,20 +7,41 @@ function GetQueryString(name)
 
 $(document).ready(function () {
     let articleID = GetQueryString("id");
+    let linkAddress = GetQueryString("linkAddress");
+    if(articleID !== undefined){
+        $.getJSON("./article_abstract.json", function (result) {
+            for(let i = 0; i < result.length; i++){
+                if($.trim(result[i].id.toString()) === $.trim(articleID)){
+                    // 读取文字
+                    readFile(result[i].address);
+                    let title = result[i].title;
+                    $("#article-title").html(title);
 
-    $.getJSON("./article_abstract.json", function (result) {
-        for(let i = 0; i < result.length; i++){
-            if($.trim(result[i].id.toString()) === $.trim(articleID)){
-                readFile(result[i].address);
-                let title = result[i].title;
-                $("#article-title").html(title);
-
-
-                break;
+                    // 读取链接
+                    if(result[i].additionalLink !== undefined){
+                        let linksHTML = "";
+                        let links = result[i].additionalLink;
+                        for(let j = 0; j < links.length; j++){
+                            let cellHTML =
+                                "<div class='linkcell' " +
+                                "onclick='redirectToLink($(this))' " +
+                                "data-value='"+links[j].linkID+"'>"
+                                + links[j].linkName +
+                                "</div>";
+                            linksHTML = linksHTML + cellHTML;
+                        }
+                        $("#additionalLinks").html(linksHTML);
+                    }
+                    break;
+                }
             }
-        }
-    })
+        })
+    }
 })
+
+function redirectToLink(obj) {
+    $(location).attr("href", "./read.html?id=" + $(obj).data("value"))
+}
 
 function readFile(filepath){
     let text = $.ajax({
--- PageLayout.js
+++ PageLayout.js
@@ -34,26 +34,27 @@ function renderArticlesAbstract() {
             let id = abstract.id;
             let title = abstract.title;
             let icon = abstract.icon;
-            let abstractText = textToHTML(abstract.abstract);
 
-            let currentHTML = '<div class="column">\n' +
-                '                <div id="'+id+'" class="article_box" onclick="abstractOnClick($(this))">\n' +
-                '                    <div class="article_box_header">\n' +
-                '                        <img class="article_box_header_ico" src="'+icons[icon]+'"/>\n' +
-                '                        <div class="article_box_header_text">\n' +
-                                            title +
-                '                        </div>\n' +
-                '                    </div>\n' +
-                '                    <div class="article_box_abstract">\n' +
-                                        abstractText+
-                '                    </div>\n' +
-                '                </div>\n' +
-                '            </div>'
+            if(icon !== "hide"){
+                let abstractText = textToHTML(abstract.abstract);
 
-            abstractsHTML += currentHTML;
+                let currentHTML = '<div class="column">\n' +
+                    '                <div id="'+id+'" class="article_box" onclick="abstractOnClick($(this))">\n' +
+                    '                    <div class="article_box_header">\n' +
+                    '                        <img class="article_box_header_ico" src="'+icons[icon]+'"/>\n' +
+                    '                        <div class="article_box_header_text">\n' +
+                    title +
+                    '                        </div>\n' +
+                    '                    </div>\n' +
+                    '                    <div class="article_box_abstract">\n' +
+                    abstractText+
+                    '                    </div>\n' +
+                    '                </div>\n' +
+                    '            </div>'
+
+                abstractsHTML += currentHTML;
+            }
         }
-
-
         $("#articlesContainer").html(abstractsHTML);
     }
 }
